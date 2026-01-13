e7f9eeb56de112beea4a2eba57ae0729080fa039
Arghadip Goswami <arghadipgoswami816@gmail.com>
2026-01-13 16:45:48 +0530

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,11 +1,5 @@
-#4) To remove all lines that contains a character a in a file and write the rest in a different file.
-
-with open("input.txt", "r") as infile:
-    with open("output.txt", "w") as outfile:
-        for line in infile:
-            # convert line to lowercase before checking
-            if 'a' not in line.lower():
-                outfile.write(line)
-
-print("Lines containing 'a' or 'A' have been removed and rest is written in output.txt")
-
+#4. Remove all the line that contain the character "a" in the file and write to another file.
+with open('input.txt','r') as src, open('output.txt','w')as dest:
+    for line in src:
+        if 'a' not in line and 'A' not in line:
+            dest.write(line)
